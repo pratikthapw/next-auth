5ac1bd916871a9d7a2d5c19c9e03af3a429ef816
Pratik <pratikthapw@gmail.com>
2024-04-10 21:04:54 +0545

    middleware updated 2

--- middleware.ts
+++ middleware.ts
@@ -1,18 +1,2 @@
-// export { default } from "next-auth/middleware";
-
-import { withAuth } from "next-auth/middleware";
-import { NextResponse } from "next/server";
-
-export default withAuth(
-  // `withAuth` augments your `Request` with the user's token.
-  function middleware(req) {
-    return NextResponse.rewrite(new URL("/dashboard", req.url));
-  },
-  {
-    callbacks: {
-      authorized: ({ token }) =>
-        token?.role === "admin" || token?.role === "seller",
-    },
-  }
-);
+export { default } from "next-auth/middleware";
 export const config = { matcher: ["/dashboard/:path*"] };
