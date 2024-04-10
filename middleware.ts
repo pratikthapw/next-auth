27b3b91c7da538340c306cf4d31bd1748aaacdf8
Pratik <pratikthapw@gmail.com>
2024-04-10 20:57:03 +0545

    feat: middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,21 +1,18 @@
-import { getToken } from "next-auth/jwt";
-import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+// export { default } from "next-auth/middleware";
 
-export async function middleware(request: NextRequest) {
-  await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
-  const cookie = request.cookies.get("next-auth.session-token");
-  const isLoggedIn = cookie?.value;
-  const isProtectedRoute = request.nextUrl.pathname.includes("/dashboard");
-  const isLoginRoute = request.nextUrl.pathname.includes("/login");
-  if (!isLoggedIn && isProtectedRoute) {
-    return NextResponse.redirect(new URL("/login", request.nextUrl.origin));
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+  // `withAuth` augments your `Request` with the user's token.
+  function middleware(req) {
+    return NextResponse.rewrite(new URL("/dashboard", req.url));
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) =>
+        token?.role === "admin" || token?.role === "seller",
+    },
   }
-  if (isLoggedIn && isLoginRoute) {
-    return NextResponse.redirect(new URL("/dashboard", request.nextUrl.origin));
-  }
-  return NextResponse.next();
-}
+);
+export const config = { matcher: ["/dashboard/:path*"] };
